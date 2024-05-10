2c88acc1f8d64c80739a1fcfa05a0fe82105cc28
Martin <martin.schreurs@netcologne.de>
2024-05-10 16:34:20 +0200

    first registry

---
+++ ports/cpt_lib/portfile.cmake
@@ -0,0 +1,19 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO PurpurTentakel97/cpt-lib
+        REF "${VERSION}"
+        SHA512 0
+        HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+)
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
