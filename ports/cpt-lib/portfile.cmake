3cb12adf2c6a53f5a0bae5ce6402151547d8e816
Martin <martin.schreurs@netcologne.de>
2024-05-10 18:08:52 +0200

    cpt-lib v1.0.0

--- ports/cpt-lib/portfile.cmake
+++ ports/cpt-lib/portfile.cmake
@@ -2,17 +2,19 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO PurpurTentakel97/cpt-lib
         REF "v${VERSION}"
-        SHA512 63267125809a63c360b01064a9df8705bee9df6508de1ea640cf8b446b6dfc8f51ff232ae5ae78c109165bcef814dd21bf14786b192f0d09ee3f9092bb18e7bd
+        SHA512 2f096245f09772f6c45c44860d3fba2b3014bba608773c1a9ab125b9a661f63210c18ba4090c5c06e6230e6fae1aeb211052823c4a6a806a4bbe71c1ad7478b1
         HEAD_REF main
 )
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+        "-Dcpt_lib_tests=OFF"
 )
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 
-vcpkg_cmake_config_fixup()
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
 
