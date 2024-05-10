316ec0b2d5f4d04193d57bad71fc64baa480cb1b
Martin <martin.schreurs@netcologne.de>
2024-05-10 17:04:08 +0200

    cpt-lib v1.0.0

--- ports/cpt-lib/portfile.cmake
+++ ports/cpt-lib/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO PurpurTentakel97/cpt-lib
         REF "v${VERSION}"
-        SHA512 0
+        SHA512 63267125809a63c360b01064a9df8705bee9df6508de1ea640cf8b446b6dfc8f51ff232ae5ae78c109165bcef814dd21bf14786b192f0d09ee3f9092bb18e7bd
         HEAD_REF main
 )
 
